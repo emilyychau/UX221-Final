0807c24a93587eeaf919c4816259f8d49c0df8aa
emilyychau <emilyychau@users.noreply.github.com>
2024-04-19 20:43:55 +0000

    deploy: 49bb38e9ec11d899452bb2baa3fe565ad4c4d316

--- wp-content/themes/ux221-final/customElements.js
+++ wp-content/themes/ux221-final/customElements.js
@@ -5,3 +5,23 @@ class CopyrightYear extends HTMLElement{
 }
 
 customElements.define("x-year", CopyrightYear);
+
+
+class TwoSidedMarket extends HTMLElement {
+	connectedCallback(){
+		this.innerHTML = `<a href="diyers-crafters" class="item">DIYers & Crafters</a>&ensp;&ensp;<a href="vendors" class="item">Kit Vendors</a>`;
+		this.style.display = 'flex';
+        this.style.justifyContent = 'center';
+		const items = this.querySelectorAll('.item');
+        items.forEach(item => {
+            item.style.border = '2px solid black';
+            item.style.margin = '2px';
+            item.style.padding = '4px';
+			item.style.borderRadius = '5px';
+            item.style.display = 'inline-block'; 
+			item.style.fontFamily = 'Montserrat, sans-serif';
+        });
+    }
+}
+
+customElements.define("x-2sided", TwoSidedMarket);
